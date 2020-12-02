6739e01485ea6ee03bcf8a36131cefef243ba1c5
Ed Paradis <eparadis@users.noreply.github.com>
2020-12-01 22:12:16 -0800

    stubs for blocks

--- main.ts
+++ main.ts
@@ -1,1 +1,38 @@
+// Ed Paradis
+// TODO
+// - use enums for picking the line because there is only a top and bottom line
+// - use shadowOptions.toString=true on console-style methods
+// - add JSDoc for help tooltips
+// NOTES
+// Defining blocks: https://makecode.com/defining-blocks
+// 
 
+
+//% groups=['lcd backpack']
+namespace LCD {
+    //% block
+    export function clear(): void {
+
+    }
+
+    //% block
+    //% line.min=1 line.max=2 line.defl=1
+    export function clearLine(line: number): void {
+
+    }
+
+    //% block
+    export function writeLine(line: number, text: string): void {
+
+    }
+
+    //% block
+    export function log(value: number): void {
+
+    }
+
+    //% block
+    export function logValue( label: string, value: number): void {
+
+    }
+}
